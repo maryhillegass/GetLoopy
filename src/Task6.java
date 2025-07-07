84f491c96bf6ebe17e66ac1195d211dec44506fb
maryhillegass <maryhillegass@gmail.com>
2025-07-06 21:35:52 -0400

    Task 6 complete

--- src/Task6.java
+++ src/Task6.java
@@ -1,7 +1,7 @@
 public class Task6 {
     public static void main(String[] args) {
         for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < 5; j++) {
+            for (int j = 0; j < 5-i; j++) {
                 System.out.print("*");
             }
             System.out.println();
